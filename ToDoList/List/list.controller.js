9c0c27369e001a8f70c3e7b3c19490d9530c29c8
aditiro <aditi.aarohini@gmail.com>
2018-10-04 02:38:17 +0530

    Adding login validations and improved table css

--- ToDoList/List/list.controller.js
+++ ToDoList/List/list.controller.js
@@ -3,21 +3,21 @@ app.controller('ItemsController',['$scope',function($scope){
     $scope.newItemName="";
     $scope.items=[];
     $scope.addItem = function (index,titleValue) {
-        console.log(index);
-        var key = Object.keys($scope.items)[index];
-        console.log(key);
-        if(key == undefined){
+        // console.log(index);
+        // var key = Object.keys($scope.items)[index];
+        // console.log(key);
+        //if(key == undefined){
             $scope.items.push({
                 id: $scope.items.length + 1,
                 title: $scope.newItemName
             });
-        }
-        else {
-            $scope.items.splice(index,1,{
-                id: index,
-                title:titleValue
-            });
-        }
+        //}
+        // else {
+        //     $scope.items.splice(index,1,{
+        //         id: index,
+        //         title:titleValue
+        //     });
+        // }
     }
 
     $scope.deleteItem = function (index) {
@@ -33,11 +33,11 @@ app.controller('ItemsController',['$scope',function($scope){
         modal.style.display = "block";
         var span = document.getElementsByClassName("close")[0];
 
-        var saveButton = document.getElementById('save-button');
+        // var saveButton = document.getElementById('save-button');
 
-        saveButton.onclick = function(index){
-            $scope.addItem(index,$scope.newItemName);
-        }
+        // saveButton.onclick = function(index){
+        //     $scope.addItem(index,$scope.newItemName);
+        // }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
--- ToDoList/Login/login.controller.js
+++ ToDoList/Login/login.controller.js
@@ -1,49 +1,54 @@
 var app =angular.module('listApp',[]);
-app.controller('ItemsController',['$scope',function($scope){
+app.controller('loginController',['$scope',function($scope){
 $scope.username="";
 $scope.password="";
+$scope.isInvalidLogin=false;
 $scope.systemUser=[{
-    key: $scope.systemUser.length + 1,
+   // key: $scope.systemUser.length + 1,
     username: "aditiro",
     password: "aditiro"
 },
 {
-    key: $scope.systemUser.length + 1,
+   // key: $scope.systemUser.length + 1,
     username: "shwetana",
     password: "shwetana"
 }];
 $scope.validateUser=function(){
+debugger;
 var keys=[];
 var usernames=[];
 for(var i=0;i<$scope.systemUser.length;i++){
-    keys.push($scope.systemUser.key);
-    usernames.push($scope.systemUser.username);
+    //keys.push($scope.systemUser.key);
+    usernames.push($scope.systemUser[i].username);
 }
 var counter=-1;
 var userKey=-1;
 for(var user in usernames){
-    if($scope.systemUser.username == usernames[user]){
-
+    if($scope.username == usernames[user]){
             userKey = counter+1;
-        
         break;
     }
     else{
         counter++;
     }
+}
     if(userKey!=-1){
         var password = $scope.systemUser[userKey].password;
         if(password != $scope.password){
             //error condition password
+            $scope.isInvalidLogin = true;
         }
         else {
             //goto next page
+            $scope.isInvalidLogin = false;
+            location.href = "../List/list.html";
         }
     }
     else {
         //error condition for user 
+        $scope.isInvalidLogin = true;
     }
-}
+
 
 }
 }]);
